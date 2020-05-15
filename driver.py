f81160255601c08e25f62e9cd7151a2567c42423
jadkins99 <jacob.adkins18@ncf.edu>
2020-05-15 19:00:01 -0400

    add to driver

--- driver.py
+++ driver.py
@@ -22,28 +22,28 @@ M = 10
 k = r/(0.5*sigma**2) # page 136
 
 
-uMatrix,xgrid = crank_nicholson(E,r,sigma,T,S_max,Nx,M,call_payoff,u_m_inf_call,u_p_inf_call)
-#uMatrix,xgrid = implicit_fd(E,r,sigma,T,S_max,Nx,M,call_payoff,u_m_inf_call,u_p_inf_call)
+#uMatrix,xgrid = crank_nicholson(E,r,sigma,T,S_max,Nx,M,call_payoff,u_m_inf_call,u_p_inf_call)
+uMatrix,xgrid = implicit_fd(E,r,sigma,T,S_max,Nx,M,call_payoff,u_m_inf_call,u_p_inf_call)
 #uMatrix,xgrid = explicit_fd(E,r,sigma,T,S_max,Nx,M,call_payoff,u_m_inf_call,u_p_inf_call)
 # transform variables into financial variables
 S = E*np.exp(xgrid)
 
 tau_max = 0.5*(sigma**2)*T
-dt = tau_max/Nx
+dt = tau_max/M
 t = 0
 
 tau = 0.5*(sigma**2)*(T-t)
-M = np.ceil(tau_max/dt)
+
 m = int(np.floor(tau/dt))
 
 # page 136
 
 V = (E**(0.5*(1+k)))*(S**(0.5*(1-k)))*(np.exp(0.125*((k+1)**2)*(sigma**2)*(T-t)))*uMatrix
 
-#if m == Nx:
-#    m -=1
+if m == M:
+    m -=1
 
-plt.plot(S,V[-1,:])
+plt.plot(S,V[m,:])
 plt.xlabel("S")
 plt.ylabel("V")
 plt.show()
